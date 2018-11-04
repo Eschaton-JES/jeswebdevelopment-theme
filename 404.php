a1bda61f810e915522c253e03280677c8db9dc70
JES WebDev <30653601+Eschaton-JES@users.noreply.github.com>
2018-11-04 23:35:04 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<h1 id="error-header">Sorry. There's nothing to be found here!</h1>
+<h1 id="error-header">Sorry. There's nothing to be found here!!</h1>
 
 <img id="error-img" src="https://jeswebdevelopment.com/wp-content/uploads/2018/10/error.png" alt="404 error!" />
 
